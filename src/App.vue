83b728143f806d721d3ac0eac907a97cea93e8a0
Joaquin Rampa <rampajoa@gmail.com>
2025-03-30 14:36:57 +0200

    portfolio created

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="yo">
+      about
+    </div>
+    <div class="content">
+      <p>Home</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>Más contenido...</p>
+      <p>a</p>
+      <p>mas</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  html, body, #app {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background-color: #0a192f;
+    color: #dfd9d9;
+  }
+
+  .container {
+    height: 100%;
+  }
+
+  .yo {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 50%;  
+    height: 100vh;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+
+  .content {
+    margin-left: 50%;
+    height: 100vh;
+    overflow-y: auto;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+</style>
